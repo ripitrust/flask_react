1e31bcf8806f7a020eb1ca5dcb4c2a368d77a325
Yang Fan <yangfan@garena.com>
2016-05-20 19:25:56 +0800

    fix entry point

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='flask_react',
-      version='0.0.1',
+      version='0.0.2',
       author='Yang Fan',
       author_email='yangfan@garena.com',
       description='Auto setup tool for flask and react project',
-      #url='https://gitsa.garenanow.com/yangfan/Ultron-agent',
+      url='git@github.com:ripitrust/flask_react.git',
       packages=['flask_react'],
       install_requires=['virtualenv'],
-      entry_points={'console_scripts': ['flask_react=project.main:main']},
+      entry_points={'console_scripts': ['flask_react=flask_react.main:main']},
 )
